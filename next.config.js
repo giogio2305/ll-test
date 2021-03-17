031728f9ebe2302ae933d8a53bfc66acfa548260
giogio2305 <79600936+giogio2305@users.noreply.github.com>
2021-03-17 11:55:47 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+      'MYSQL_HOST': '127.0.0.1',
+      'MYSQL_PORT': '3306',
+      'MYSQL_DATABASE': "geo",
+      'MYSQL_USER': "moo",
+      'MYSQL_PASSWORD': "Db12345678",
+    }
+  }
