81154200a69b7b2f8ae5e281bb6be2c47b26996d
giogio2305 <79600936+giogio2305@users.noreply.github.com>
2021-03-17 13:40:07 +0100

    Update signin.js

--- pages/signin.js
+++ pages/signin.js
@@ -1,16 +1,19 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-import { gql, useMutation } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
+import { useMutation, useApolloClient } from '@apollo/client'
 import { getErrorMessage } from '../lib/form'
 import Field from '../components/field'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const SignUpMutation = gql`
-  mutation SignUpMutation($pseudo: String!, $email: String!, $password: String!) {
-    signUp(input: { pseudo: $pseudo, email: $email, password: $password }) {
+
+const SignInMutation = gql`
+  mutation SignInMutation($pseudo: String!, $email: String!, $password: String!) {
+    signIn(input: { pseudo: $pseudo, email: $email, password: $password }) {
       user {
         id
         pseudo
@@ -20,36 +23,46 @@ const SignUpMutation = gql`
   }
 `
 
-function SignUp() {
-  const [signUp] = useMutation(SignUpMutation)
+
+function SignIn() {
+  const client = useApolloClient()
+  const [signIn] = useMutation(SignInMutation)
   const [errorMsg, setErrorMsg] = useState()
   const router = useRouter()
+ 
 
   async function handleSubmit(event) {
     event.preventDefault()
+
     const pseudoElement = event.currentTarget.elements.pseudo
     const emailElement = event.currentTarget.elements.email
     const passwordElement = event.currentTarget.elements.password
+    
 
     try {
-      await signUp({
+      await client.resetStore()
+      const { data } = await signIn({
         variables: {
           pseudo: pseudoElement.value,
           email: emailElement.value,
           password: passwordElement.value,
         },
-      })
+      });
+      console.log(data);
+      if (data.signIn.user) {
+        await router.push('/profile')
 
-      router.push('/signin')
+      }
     } catch (error) {
-      console.log(error);
       setErrorMsg(getErrorMessage(error))
+      console.log(error);
     }
   }
 
   return (
     <>
-      <div className={styles.container}>
+    
+<div className={styles.container}>
 <Head>
         <title>Leloux employ&eacute;</title>
         <link rel="icon" href="/favicon.ico" />
@@ -58,7 +71,7 @@ function SignUp() {
  <div className={styles.aff}></div>
  <div className={styles.form}>
    <div className={styles.spc}></div>
-   <h2>Inscrivez-vous sur <span className={styles.ll}>Leloux</span></h2>
+   <h2>Connectez-vous à <span className={styles.ll}>Leloux</span></h2>
    {errorMsg && <p>{errorMsg}</p>}
    <div>
      <form onSubmit={handleSubmit}>
@@ -93,7 +106,7 @@ alt="lock"
 placeholder="8+ caract&egrave;res"
 />
        
-       <input type="submit" className={styles.ib} value="S'inscrire"></input>
+       <input type="submit" className={styles.ib} value="Connexion"></input>
      </form>
    </div>
  </div>
@@ -103,4 +116,4 @@ placeholder="8+ caract&egrave;res"
   )
 }
 
-export default SignUp
+export default SignIn
